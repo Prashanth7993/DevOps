7d1901c641ce0bee0fc4c7be841212116d421753
RASHANTH793 <jprashanth429@gmail.com>
2025-01-29 16:46:17 +0530

    Adding a new folder Assignemnt

---
+++ Day2/Assignment/02-StringPalindrome.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#Author: Prashanth
+#Purpose: Converting Lower to upper
+# shellcheck disable=SC2113
+#Usage: ./01-ConvertLowerUpper.sh
+
+echo "Enter the String: "
+read -r Str
+myvarlen=${#Str}
+rev=""
+for(( i=$myvarlen-1; i>=0; i-- )); do
+    rev="$rev${Str:i:1}"
+done
+if [ $Str == $rev ]; then
+    echo "It is a palindrome"
+else
+    echo "It is not a palindrome"
+fi
