8e13cbd6811959f621921d184cc950ac942803be
RASHANTH793 <jprashanth429@gmail.com>
2025-01-30 09:37:03 +0530

    Script for Counting the words in the string

---
+++ Day2/Assignment/03-CountNumberOfWordsinString.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Author: Prashanth
+#Purpose: Count Number of Word in String
+# shellcheck disable=SC2113
+#Usage: ./03-CountNumberOfWordsinString.sh
+
+echo -n "Enter the String: "
+read -r Str
+echo "$#"
