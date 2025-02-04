d55240429fc5af8def74125623d87d09349e3fb5
RASHANTH793 <jprashanth429@gmail.com>
2025-02-04 14:11:02 +0530

    Updated Python code

--- Python/Day1/01-Basic.py
+++ Python/Day1/01-Basic.py
@@ -1,35 +1,90 @@
 "Hii hello world"
 Hello="Hii hello world"
 
-print("Hello world")
-print(3+2)
-print(3**3)
-print(3//2)
+# print("Hello world")
+# print(3+2)
+# print(3**3)
+# print(3//2)
 
 
 # int
-print(type(3))
+# print(type(3))
 
 #Float
-print(type(3.14))
+# print(type(3.14))
 
 #Complex
-print(type(1+3j))
+# print(type(1+3j))
 
 #list
 Ls = [1,2,3,4,5]
-print(type(Ls))
-print(Ls)
+# print(type(Ls))
+# print(Ls)
 
 #Dict
 test = {1:2,2:9,3:4}
-print(type(test))
+# print(type(test))
 
 #Set (Duplicates will not allow)
 test1 = {1234,2344543}
-print(type(test1))
+# print(type(test1))
 
 #Tuple
 tup = (1,2,3,4.5)
-orint(type(tup))
+# print(type(tup))
+
+
+
+FName = 'J'
+LName = 'Prashanth'
+Age = 22
+
+PerInfo = {'FName': 'J','LName':'Prashanth','Age':22}
+# print(PerInfo)
+# print(len(LName))
+# print(len(PerInfo))
+
+#Functions
+#len
+
+
+
+
+x=5
+# x**=3
+# x//=3
+# x&=3
+# print(x)
+
+length = 12
+width = 10
+area = length*width
+# print(area)
+
+
+# print(3>=2 & 4>=1)
+# print(3>=2 and 4>=1)
+#print((3>=2) & (4>=1))
+
+#print((3<=2) or (4>=1))
+
+#print([3<=2])
+
+
+#Strings#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
