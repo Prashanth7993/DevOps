9930b89eb3787541b1e2db07418f838008b5c04a
RASHANTH793 <jprashanth429@gmail.com>
2025-02-04 11:34:36 +0530

    Adding New Folder Python With Da1 Notes and Basic Concept

---
+++ Python/Day1/01-Basic.py
@@ -0,0 +1,35 @@
+"Hii hello world"
+Hello="Hii hello world"
+
+print("Hello world")
+print(3+2)
+print(3**3)
+print(3//2)
+
+
+# int
+print(type(3))
+
+#Float
+print(type(3.14))
+
+#Complex
+print(type(1+3j))
+
+#list
+Ls = [1,2,3,4,5]
+print(type(Ls))
+print(Ls)
+
+#Dict
+test = {1:2,2:9,3:4}
+print(type(test))
+
+#Set (Duplicates will not allow)
+test1 = {1234,2344543}
+print(type(test1))
+
+#Tuple
+tup = (1,2,3,4.5)
+orint(type(tup))
+
