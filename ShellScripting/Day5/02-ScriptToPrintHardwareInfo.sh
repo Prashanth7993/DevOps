0064f40ad4369a60df2618616643ca44e0f9dd39
RASHANTH793 <jprashanth429@gmail.com>
2025-02-06 12:53:40 +0530

    Udpated Code To print Hardware info

--- ShellScripting/Day5/02-ScriptToPrintHardwareInfo.sh
+++ ShellScripting/Day5/02-ScriptToPrintHardwareInfo.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 #Author: Prashanth
 #Purpose: A Shell Script file to print hardware info file.
+# shellcheck disable=SC2113
 #Usage: ./02-ScriptToPrintHardwareInfo.sh
+
+cpuinfo=$(lscpu)
+echo "Cpu Details $cpuinfo"
+
+hwinfo=$(lshw)
+echo "Hardware details $hwinfo"
+
+exe=$(free -h)
+echo "free command $exe"
+
+uname=$(uname -a)
+echo "uname command $uname"
+
+meminfo=$(sudo cat /proc/meminfo)
+echo "meminfo command $meminfo"
+
+disk=$(lsblk)
+echo "diskinfo command $disk"
+
