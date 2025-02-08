0625b3924b6a4c05179abc933d27f7ec4c74b577
RASHANTH793 <jprashanth429@gmail.com>
2025-02-08 17:17:16 +0530

    Adding a Folder Modules

---
+++ Python/Day3/Modules/file1.py
@@ -0,0 +1,26 @@
+def Sum(num1,num2):
+    print(num1 + num2)
+
+
+person = {"fname":"prashanth",
+          "lname":"kumar",
+          "age":21,
+          "Aid":123456,
+          "friends":["skc","ak"],
+          "skills":{
+              "backed_skills": ["nodejs", "ruby"],
+              "database":["redis","mysql","postgress"]
+          }
+        }
+person["address"]="mathura"
+print(person)
+print(person.get("skills").get("backed_skills"))
+print(person.get("address"))
+print("address" in person)
+print(person.pop("address"))
+print(len(person))
+del person["friends"]
+print(len(person))
+print(person["skills"]["backed_skills"])
+person["skills"]["database"].append("mongodb")
+print(person["skills"]["database"])
---
+++ Python/Day3/Modules/file2.py
@@ -0,0 +1,4 @@
+from file1 import Sum as sum , person as p
+
+print(sum(2,5))
+print(p)
