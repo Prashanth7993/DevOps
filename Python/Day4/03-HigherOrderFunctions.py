31ba976816153c25290b9ecf3f09aba6430ad229
RASHANTH793 <jprashanth429@gmail.com>
2025-02-10 15:20:42 +0530

    Updated Code Higher Order Function

--- Python/Day4/03-HigherOrderFunctions.py
+++ Python/Day4/03-HigherOrderFunctions.py
@@ -1,10 +1,10 @@
 def Sum_num(x):
     return sum(x)
 
-def Higher_orderFunction(f,lst):
+def Higher_orderFunction(f,lst): #f = Sum_num
     var = f(lst)
     return var
 
 result = Higher_orderFunction(Sum_num,[1,2,3,4,5])
-print(result)
+# print(result)
 
