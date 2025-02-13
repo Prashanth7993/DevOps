50dfce2f791dba38e61ea5ea28c77aa6ae5f83d3
RASHANTH793 <jprashanth429@gmail.com>
2025-02-13 14:07:59 +0530

    Uploading New Day7 Folder with Python code an testing

---
+++ Python/Day7/test_pytest1.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+
+def test_method():
+    driver = webdriver.Chrome()
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    elem = driver.find_element(By.NAME, "q")
+    elem.clear()
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    assert "No results found." not in driver.page_source
+    time.sleep(10)
+    driver.close()
+    print("test passed")
+
